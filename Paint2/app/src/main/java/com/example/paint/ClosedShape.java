22ed91802561c9fe58af187b0ef7e1e45e410044
yehonatan1112 <99614750+yehonatan1112@users.noreply.github.com>
2022-02-13 23:44:57 +0200

    Paint v1

---
+++ Paint2/app/src/main/java/com/example/paint/ClosedShape.java
@@ -0,0 +1,26 @@
+package com.example.paint;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class ClosedShape extends Shape{
+
+
+    public ClosedShape(int x, int y, String color,int width,boolean fill) {
+        super(x, y, color,width,fill);
+
+    }
+
+    @Override
+    public void updatePoint(int xe, int ye) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas, Paint paint) {
+        super.draw(canvas,paint);
+    }
+    public double GetSurface(){
+        return 0;
+    }
+}
